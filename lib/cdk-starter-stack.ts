ae17f60aef569b6bcd3eae0993fec7904c04e143
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-06-14 09:46:59 +0300

    add photos stack

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,16 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [{
+        expiration: cdk.Duration.days(expiration)
+      }]
+    });
+  }
+}
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: 2,
+          status: 'Enabled'
+        }]
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const duration = new cdk.CfnParameter(this, 'duration', {
+      default: 6,
+      minValue: 1,
+      maxValue: 10,
+      type: 'Number'
+    });
+
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [{
+        expiration: cdk.Duration.days(duration.valueAsNumber)
+      }]
+    });
+    // console.log('bucket name: ', myL2Bucket.bucketName);
+
+    new cdk.CfnOutput(this, 'MyL2BucketName', {
+      value: myL2Bucket.bucketName
+    });
+
+    new L3Bucket(this, 'MyL3Bucket', duration.valueAsNumber);
   }
 }
