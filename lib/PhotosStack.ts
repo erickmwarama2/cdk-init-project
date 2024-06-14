7d3fa6310f9dc0c3afadacd9f95b8510e1200ed6
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-06-14 10:18:29 +0300

    rename bucket

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -6,6 +6,8 @@ export class PhotosStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        new Bucket(this, 'PhotosBucket');
+        new Bucket(this, 'PhotosBucket2', {
+            bucketName: 'photosbucket-1234em'
+        });
     }
 }
